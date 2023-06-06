df5b32df0dd98003935f42a5b6ed434d6acd0917
Potter <potteramylynn@gmail.com>
2023-06-05 21:59:34 -0500

    deleted comments, and updated readme.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # PWA Text Editor
 ## About
+Jate, or Just Another Text Editor, is a progressive web application users can run in their browser, or install for offline use. 
+For this progressive web application, I was given starter code and had to update the service worker, database, and install, and web-pack. In the web-pack, I installed the plugins. For install.js I added code to hide the install button. In database.js, I updated routes. In my service worker file, I updated the register route and enabled plugins.
 ## Screenshots
+![Screenshot of the deployed site](./client/src/images/pwa-text-editor-ap.herokuapp.com_.png)
+## Link to Live Site
+[Link to depolyed site on Heroku](https://pwa-text-editor-ap.herokuapp.com/)
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,9 +1,7 @@
-/*
-  The idb package being required below provides some syntactic sugar around the methods needed to work with IndexedDB. Yes, the code you see below is actually a "prettier" version of what you would normally have to write. Be thankful. We've only been using the idb package since mid 2022. Before that students had to write this code with no helper methods. These students deserve a medal.
-*/
+
 import { openDB } from 'idb';
 
-// We will define a global constant for our database name so we don't mess it up anywhere
+
 const jate = "jate"
 
 const initdb = async () =>
@@ -18,19 +16,14 @@ const initdb = async () =>
     },
   });
 
-/*
-  We need to add some code below which will take updated content and save it to IndexedDB.
-*/
+
 export const putDb = async (content) => {
-  // First, create a variable, and set it to asyncronously await the opening of the database. Replace the items in all caps
-  
-  // TODO: Change YOUR_OPEN_DB_VAR to whatever variable name you wanT. Note that you'll then need to change any other occcurences of YOUR_OPEN_DB_VAR to the same variable name.
+
   const openJate = await openDB(jate, 1);
 
-  // TODO: Now create a variable for the transaction; again, this will be referenced below.
+
   const transaction = openJate.transaction(jate, 'readwrite');
 
-  // TODO: Now create a variable for the store
   const store = transaction.objectStore(jate);
 
   const request = store.put({ id: 1, value: content });
@@ -38,18 +31,14 @@ export const putDb = async (content) => {
   console.log('🚀 - data saved to the database', result.value);
 };
 
-/*
-  We need to add some code below which will get all content from IndexedDB.
-*/
+
 export const getDb = async () => {
-  // You can duplicate the same lines of code from above, except that the transaction will be 'readonly'
-  
-  // TODO: Copy LINES 28, 31 and 34 above; the new line 31 code should be "readonly"
+ 
   const openJate = await openDB(jate, 1);
   const transaction = openJate.transaction(jate, 'readonly');
   const store = transaction.objectStore(jate);
 
-  // Leave the rest as-is
+
   const request = store.get(1);
   const result = await request;
   result
